c5889a6569a63960455947e65f3d990fc1b2c752
cap-nascimento <anascimentovieira3@gmail.com>
2024-09-02 22:32:36 -0300

    add card difficulty mock

--- db.files/gc-ready.sql
+++ db.files/gc-ready.sql
@@ -15,7 +15,19 @@ drop table Cards;
 
 -- Card Difficulty
 INSERT INTO Card_Difficulty (Difficulty_Description) VALUES ("SS");
+INSERT INTO Card_Difficulty (Difficulty_Description) VALUES ("A");
+INSERT INTO Card_Difficulty (Difficulty_Description) VALUES ("B");
+INSERT INTO Card_Difficulty (Difficulty_Description) VALUES ("C");
+INSERT INTO Card_Difficulty (Difficulty_Description) VALUES ("D");
+INSERT INTO Card_Difficulty (Difficulty_Description) VALUES ("E");
+INSERT INTO Card_Difficulty (Difficulty_Description) VALUES ("F");
+INSERT INTO Card_Difficulty (Difficulty_Description) VALUES ("G");
+INSERT INTO Card_Difficulty (Difficulty_Description) VALUES ("H");
 
--- Cards
+-- Cards (000 - 099)
 INSERT INTO Cards (Title, Quantity, Description, Slot_Number, Difficulty_Code) VALUES ("Ruler's Blessings", 1, "A Castle given as a prize for winning the contest, town with population 10.000 included. It's Residents will live according to whatever laws and commands you issue.", "000", 1);
 INSERT INTO Cards (Title, Quantity, Description, Slot_Number, Difficulty_Code) VALUES ("Patch of Forest", 3, "The entrance to the giant forest called the Mountain god's garden where many unique endemic species live. They are all tame and friendly.", "001", 1);
+
+-- Type Question : Movies, Animes, Música
+
+-- Question 
